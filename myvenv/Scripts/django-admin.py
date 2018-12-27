7e9a83210f532c422d7d5ef8a4ce31ae542a9bc4
Benjamin Ocampo <rurik@ualberta.ca>
2018-12-27 12:55:19 -0700

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lingbo\documents\power_bi_workspace\ben_workspace\python_webdev\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
